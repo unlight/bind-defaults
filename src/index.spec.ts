d7b33682876ae425b35f8660651eeb19e66d860b
Roman <s91630277@gmail.com>
2025-02-16 13:45:24 +0400

    chore: Improve typings

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,43 @@
-import { describe, expect, it } from 'vitest';
+import { expect, it } from 'vitest';
+
 import { bindDefaults } from './index';
 
 it('first bind', () => {
-  const fn = ({ a }) => a;
-  const boundFunction = bindDefaults(fn, { a: 'A' });
-  const result = boundFunction.apply(null, [] as any);
+  const testFunction = ({ a }: { a?: string }) => a;
+  const boundFunction = bindDefaults(testFunction, { a: 'A' });
+  const result = boundFunction.call(null, {});
   expect(result).toEqual('A');
+  expect(boundFunction.name).toEqual('testFunction');
+});
+
+it('first bind with call', () => {
+  const testFunction = ({ a }: { a?: string }) => a;
+  const boundFunction = bindDefaults(testFunction, { a: 'A' });
+  const result = boundFunction({ a: 'Bound A' });
+  expect(result).toEqual('Bound A');
+});
+
+it('second bind', () => {
+  const testFunction = ({ a, b }: { a?: string; b?: string }) => ({ a, b });
+  const boundFunction1 = bindDefaults(testFunction, { a: 'A' });
+  const boundFunction2 = bindDefaults(boundFunction1, { b: 'B' });
+  const result = boundFunction2({});
+  expect(result).toEqual({ a: 'A', b: 'B' });
+});
+
+it('triple bind', () => {
+  const testFunction = ({
+    a,
+    b,
+    c,
+  }: {
+    a?: string;
+    b?: string;
+    c?: string;
+  }) => ({ a, b, c });
+  const boundFunction1 = bindDefaults(testFunction, { a: 'A' });
+  const boundFunction2 = bindDefaults(boundFunction1, { b: 'B' });
+  const boundFunction3 = bindDefaults(boundFunction2, { a: 'A3', c: '3' });
+  const result = boundFunction3({});
+  expect(result).toEqual({ a: 'A3', b: 'B', c: '3' });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-export function bindDefaults(originalFunction: Function, defaults: any) {
+type FirstParameter<T extends (...args: any[]) => any> = Parameters<T>[0];
+type TailParameters<T extends (...args: any[]) => any> = T extends (
+  first: any,
+  ...rest: infer Rest
+) => any
+  ? Rest
+  : never;
+
+export function bindDefaults<
+  F extends (
+    options: FirstParameter<F>,
+    ...args: TailParameters<F>
+  ) => ReturnType<F>,
+>(originalFunction: F, defaults: FirstParameter<F>) {
   const originalFunctionName = originalFunction.name;
-  const boundFunction = function (this: unknown, options: any, ...args: any) {
-    const mergedOptions = Object.assign({}, defaults, options);
+  const boundFunction = function (
+    this: unknown,
+    options: FirstParameter<F>,
+    ...args: TailParameters<F>
+  ) {
+    const mergedOptions: FirstParameter<F> = Object.assign(
+      {},
+      defaults,
+      options,
+    );
 
     return originalFunction.call(this, mergedOptions, ...args);
   };
@@ -10,48 +31,3 @@ export function bindDefaults(originalFunction: Function, defaults: any) {
 
   return boundFunction;
 }
-
-// type BoundFunc<F extends (options: Options, ...args: any[]) => any> = F & {
-//   defaults: Options;
-//   bindDefaults: (newDefaults: Options) => BoundFunc<F>;
-//   [isBoundFuncSymbol]: true;
-//   [originalFuncSymbol]: F;
-// };
-
-// const isBoundFuncSymbol = Symbol('isBoundFunc');
-// const originalFuncSymbol = Symbol('originalFunc');
-
-// function bindWithDefaults<F extends (options: Options, ...args: any[]) => any>(
-//   func: F,
-//   defaults: Options = {},
-// ): BoundFunc<F> {
-//   let originalFunc: F = func;
-//   let existingDefaults: Options = {};
-
-//   if (typeof func === 'function' && (func as any)[isBoundFuncSymbol]) {
-//     existingDefaults = (func as BoundFunc<F>).defaults;
-//     originalFunc = (func as BoundFunc<F>)[originalFuncSymbol];
-//   }
-
-//   const mergedDefaults = { ...existingDefaults, ...defaults };
-
-//   const boundFunc = ((options: Options = {}, ...args: any[]) => {
-//     return originalFunc({ ...mergedDefaults, ...options }, ...args);
-//   }) as BoundFunc<F>;
-
-//   Object.defineProperties(boundFunc, {
-//     [isBoundFuncSymbol]: { value: true, enumerable: false },
-//     defaults: { value: mergedDefaults, enumerable: false },
-//     [originalFuncSymbol]: { value: originalFunc, enumerable: false },
-//     bindDefaults: {
-//       value: (newDefaults: Options) => bindWithDefaults(boundFunc, newDefaults),
-//       enumerable: false,
-//     },
-//   });
-
-//   return boundFunc;
-// }
-
-// export { bindWithDefaults, isBoundFuncSymbol, originalFuncSymbol };
-
-// type Options = Record<string, unknown>;
